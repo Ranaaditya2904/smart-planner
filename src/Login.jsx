c8ed608691ef312b9e5a21fc8ef8f50297d7545c
Aditya Kumar Rana <ranaaditya2904@gmail.com>
2026-04-02 00:12:23 +0530

    "updated backend url"

--- src/Login.jsx
+++ src/Login.jsx
@@ -8,7 +8,7 @@ export default function Login({ onLogin, goToSignup }) {
  const handleLogin = async () => {
   try {
     const res = await axios.post(
-      "http://localhost:5000/api/auth/login",
+      "https://focus-backend-vj3r.onrender.com/api/auth/login",
       {
         email,
         password
